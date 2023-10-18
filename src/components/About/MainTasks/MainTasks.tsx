38ea1d157624f57a71387a1da62ad6378164e8bf
Tetiana Tsar <protetsar@gmail.com>
2023-10-18 20:06:40 +0300

    fix tabs

--- src/components/About/MainTasks/MainTasks.tsx
+++ src/components/About/MainTasks/MainTasks.tsx
@@ -5,18 +5,18 @@ import { Container, SectionWrapper, Title, TaskList, TaskIcon,
 import taskList from "./taskList";
 
 const MainTasks: React.FC = () => {
-    return <Container>
-        <SectionWrapper>
-            <Title>Основні завдання</Title>
-            <TaskList>
-                {taskList.map(({ id, icon, desc }) => {
-                    return <Task key={id}>
-                        <TaskIcon src={icon} alt="task icon" />
-                        <TaskDesc>{desc}</TaskDesc>
-                    </Task>;
-                })}
-            </TaskList>
-        </SectionWrapper>
-    </Container>;
+  return <Container>
+    <SectionWrapper>
+      <Title>Основні завдання</Title>
+      <TaskList>
+        {taskList.map(({ id, icon, desc }) => {
+          return <Task key={id}>
+            <TaskIcon src={icon} alt="task icon" />
+            <TaskDesc>{desc}</TaskDesc>
+          </Task>;
+        })}
+      </TaskList>
+    </SectionWrapper>
+  </Container>;
 };
 export default MainTasks;
