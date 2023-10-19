cf86eb2e607bc5ea96835fc7565c98ef3ba48faf
Lethteria <Lethteria@gmail.com>
2023-10-19 17:38:04 +0300

    refactor: refactor theme

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,34 +2,34 @@ const themeHeaders = {
   h1: {
     fontSize: "2.5rem",
     fontWeight: 700,
-    "@media (min-width:600px)": {
+    "@media (min-width:576px)": {
       fontSize: "4rem",
     },
   },
   h2: {
     fontSize: "1.5rem",
     fontWeight: 600,
-    "@media (min-width:600px)": {
+    "@media (min-width:576px)": {
       fontSize: "2rem",
     },
-    "@media (min-width:900px)": {
+    "@media (min-width:768px)": {
       fontSize: "2.5rem",
     },
   },
   h3: {
     fontSize: "1.25rem",
     fontWeight: 600,
-    "@media (min-width:600px)": {
+    "@media (min-width:576px)": {
       fontSize: "1.5rem",
     },
-    "@media (min-width:900px)": {
+    "@media (min-width:768px)": {
       fontSize: "2rem",
     },
   },
   h4: {
     fontSize: "1.125rem",
     fontWeight: 600,
-    "@media (min-width:900px)": {
+    "@media (min-width:768px)": {
       fontSize: "1.5rem",
     },
   },
