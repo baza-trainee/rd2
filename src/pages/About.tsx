6dc265adaf60fe4e1ed670b9773bbbdb1d9d78a2
VadimToma <https://github.com/TomaVadim>
2023-10-21 14:50:35 +0300

    feat: create section container

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,14 @@
 import MainTasks from "components/About/MainTasks/MainTasks";
 import Advantages from "components/About/Advantages/Advantages";
 
-import {Test} from "../components/Test/Test";
+import { Test } from "components/Test/Test";
 
 export const About = () => {
-  return <>
-    <MainTasks />
-    <Advantages />
-    <Test />
-  </>;
+  return (
+    <>
+      <MainTasks />
+      <Advantages />
+      <Test />
+    </>
+  );
 };
