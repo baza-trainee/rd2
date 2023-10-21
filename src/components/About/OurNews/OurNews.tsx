7fc9e3b7075e7312e09d1b2b3512436769dd5b49
VadimToma <https://github.com/TomaVadim>
2023-10-21 19:09:26 +0300

    feat: update our news with list of news

--- src/components/About/OurNews/OurNews.tsx
+++ src/components/About/OurNews/OurNews.tsx
@@ -1,3 +1,5 @@
+import { ListOfNews } from "features/components/ListOfNews/ListOfNews";
+import { list } from "components/About/OurNews/listNews";
 import { SectionContainer } from "features/components/SectionContainer/SectionContainer";
 import { SectionHeader } from "features/components/SectionHeader/SectionHeader";
 
@@ -9,6 +11,7 @@ export const OurNews = () => {
         та правил безпеки. Наша команда експертів завжди готова надати вам консультацію та
         допомогти з будь-якими питаннями, пов'язаними з дронами.
       </SectionHeader>
+      <ListOfNews list={list} />
     </SectionContainer>
   );
 };
