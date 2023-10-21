f7e6cb20c6fb4777a91402413c750d091570d8ab
VadimToma <https://github.com/TomaVadim>
2023-10-21 14:53:06 +0300

    feat: create component our news

---
+++ src/components/About/OurNews/OurNews.tsx
@@ -0,0 +1,14 @@
+import { SectionContainer } from "features/components/SectionContainer/SectionContainer";
+import { SectionHeader } from "features/components/SectionHeader/SectionHeader";
+
+export const OurNews = () => {
+  return (
+    <SectionContainer>
+      <SectionHeader title="Наші новини">
+        Ми також надаємо корисні поради щодо вибору дрона, використання його можливостей
+        та правил безпеки. Наша команда експертів завжди готова надати вам консультацію та
+        допомогти з будь-якими питаннями, пов'язаними з дронами.
+      </SectionHeader>
+    </SectionContainer>
+  );
+};
