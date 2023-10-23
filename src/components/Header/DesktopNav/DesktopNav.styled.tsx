fe41bdd3cdaa4b3d5961954a6bafdcc50fc5bafc
Lethteria <Lethteria@gmail.com>
2023-10-24 00:26:14 +0300

    feat: add hover and active styles to nav and language menu

--- src/components/Header/DesktopNav/DesktopNav.styled.tsx
+++ src/components/Header/DesktopNav/DesktopNav.styled.tsx
@@ -2,21 +2,40 @@ import styled from "@emotion/styled";
 
 import {Nav} from "../../Nav/Nav";
 
+import {theme} from "../../../theme/theme";
+
+const hoverLinkColor = theme.palette.primary.dark;
+const linkColor = theme.palette.primary.main;
+
 const NavContainer = styled.div`
     display: none;
-  @media (min-width: 1440px) {
+  @media (min-width: 1280px) {
     display: flex;
   }
 `;
 
 const NavStyled = styled(Nav)`
   display: flex;
-  width: 670px;
+  width: 600px;
   justify-content: space-between;
   
   a {
     font-weight: 600;
     color: #151515;
+
+    transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  }
+
+  a.active {
+    color: ${linkColor};
+  }
+  
+  a:hover {
+    color: ${hoverLinkColor};
+  }
+
+  @media (min-width: 1440px) {
+    width: 670px;
   }
 `;
 
--- src/components/Header/LanguageMenu/LanguageMenu.styled.tsx
+++ src/components/Header/LanguageMenu/LanguageMenu.styled.tsx
@@ -1,5 +1,9 @@
 import styled from "@emotion/styled";
 
+import {theme} from "../../../theme/theme";
+
+const bgrColor = theme.palette.secondary.main;
+const textColor = theme.palette.primary.dark;
 interface LanguageMenuWrapProp {
     opened: boolean
 }
@@ -9,12 +13,24 @@ const LanguageMenuWrap = styled.div<LanguageMenuWrapProp>`
       
       font-size: 1rem;
       color: #151515;
+      background-color: ${props => (props.opened ? bgrColor : "transparent")};
+      border-radius: 5px 5px 0 0;
       
       & svg {
         transform: ${props => (props.opened ? "rotate(180deg)" : "rotate(0deg)")};
         transition: transform 300ms
       }
     }
+    & > button {
+      &:hover {
+        color: ${textColor};
+        background-color: transparent;
+      }
+    }
+  
+  & li {
+    padding: 30px 4px;
+  }
 `;
 
 export {LanguageMenuWrap};
