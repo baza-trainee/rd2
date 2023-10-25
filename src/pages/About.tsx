951c2c2c572b7c9c34c859d147275731c8339a3e
VadimToma <https://github.com/TomaVadim>
2023-10-25 09:24:13 +0300

    feat: update page about with partners

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,6 +3,7 @@ import Advantages from "components/About/Advantages/Advantages";
 
 import { Test } from "components/Test/Test";
 import { OurNews } from "components/About/OurNews/OurNews";
+import { Partners } from "components/About/Partners/Partners";
 
 export const About = () => {
   return (
@@ -10,6 +11,7 @@ export const About = () => {
       <MainTasks />
       <Advantages />
       <OurNews />
+      <Partners />
       <Test />
     </>
   );
