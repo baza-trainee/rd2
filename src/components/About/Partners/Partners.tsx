300d90a08dafc08390fd8e67e73de9caba3b0793
VadimToma <https://github.com/TomaVadim>
2023-10-25 09:19:30 +0300

    feat: create component partners

---
+++ src/components/About/Partners/Partners.tsx
@@ -0,0 +1,12 @@
+import { SectionContainer } from "features/components/SectionContainer/SectionContainer";
+import { SectionHeader } from "features/components/SectionHeader/SectionHeader";
+
+export const Partners = () => {
+  return (
+    <SectionContainer>
+      <SectionHeader title="Партнерські організації">
+        Організації, з якими ми постійно співпрацюємо
+      </SectionHeader>
+    </SectionContainer>
+  );
+};
