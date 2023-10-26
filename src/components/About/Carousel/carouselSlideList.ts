38f04be103e96d9dafc4bc4754f306bb648c085c
Lethteria <Lethteria@gmail.com>
2023-10-26 16:18:13 +0300

    refactor: del img

--- src/components/About/Carousel/carouselSlideList.ts
+++ src/components/About/Carousel/carouselSlideList.ts
@@ -6,7 +6,9 @@
 //import slide3Img from "assets/images/carousel/flying-drone-background3.jpg";
 //import slide3ImgRet from "assets/images/carousel/flying-drone-background3@2x.jpg";
 
-//import slide1Img from "../../../assets/images/carousel/flying-drone-background1.jpg";
+import slide1Img from "../../../assets/images/carousel/flying-drone-background1.jpg";
+import slide2Img from "../../../assets/images/carousel/flying-drone-background2.jpg";
+import slide3Img from "../../../assets/images/carousel/flying-drone-background3.jpg";
 
 import {CarouselSlide} from "../../../types/typeCarousel";
 
@@ -15,19 +17,19 @@ const carouselSlideList: CarouselSlide[] = [
   {
     title: "Напрямки роботи ДП",
     description: "Отримайте всі переваги членства в нашій організації - від інформації про заходи до швидкого рецензування наукових робіт",
-    //imgSrc: slide1Img,
+    imgSrc: slide1Img,
     //retinaImgSrc: slide1ImgRet,
   },
   {
     title: "Напрямки роботи ДП",
     description: "Отримайте всі переваги членства в нашій організації - від інформації про заходи до швидкого рецензування наукових робіт",
-    //imgSrc: slide2Img,
+    imgSrc: slide2Img,
     //retinaImgSrc: slide2ImgRet,
   },
   {
     title: "Напрямки роботи ДП",
     description: "Отримайте всі переваги членства в нашій організації - від інформації про заходи до швидкого рецензування наукових робіт",
-    //imgSrc: slide3Img,
+    imgSrc: slide3Img,
     //retinaImgSrc: slide3ImgRet,
   },
 ];
