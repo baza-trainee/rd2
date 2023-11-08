7c930674821b752b9475d3cb67e8dbefb1bc528f
VadimToma <https://github.com/TomaVadim>
2023-11-08 15:26:56 +0200

    feat: update restore page with description

--- src/pages/Restore.tsx
+++ src/pages/Restore.tsx
@@ -1,10 +1,15 @@
 import { AuthContainer } from "features/components/auth/AuthContainer/AuthContainer";
 import { AuthTitle } from "features/components/auth/AuthTitle/AuthTitle";
+import { Description } from "features/components/restore/Description/Description";
 
 export const Restore = (): JSX.Element => {
   return (
     <AuthContainer>
       <AuthTitle>Відновити пароль</AuthTitle>
+      <Description>
+        Для відновлення паролю введіть Вашу електронну адресу. Вам надійде лист із
+        посиланням для зміни паролю
+      </Description>
     </AuthContainer>
   );
 };
