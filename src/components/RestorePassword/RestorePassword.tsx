b2eb4b1d8befe5c78a45d09465899bab3f0809cd
VadimToma <https://github.com/TomaVadim>
2023-11-08 18:22:43 +0200

    feat: update restore with restore password

---
+++ src/components/RestorePassword/RestorePassword.tsx
@@ -0,0 +1,41 @@
+import * as Yup from "yup";
+import { FormikHelpers } from "formik";
+
+import { AuthContainer } from "features/components/auth/AuthContainer/AuthContainer";
+import { AuthTitle } from "features/components/auth/AuthTitle/AuthTitle";
+import { Description } from "features/components/restore/Description/Description";
+import { RestoreForm } from "features/components/restore/RestoreForm/RestoreForm";
+import { EmailField } from "features/components/restore/EmailField/EmailField";
+import { SubmitButton } from "features/components/auth/SubmitButton/SubmitButton";
+import { FormContainer } from "features/components/restore/FormContainer/FormContainer";
+
+export const RestorePassword = (): JSX.Element => {
+  const validationSchema = Yup.object({
+    email: Yup.string().email("введіть вірний email").required("введіть вшрний email"),
+  });
+
+  const handleSubmit = (
+    values: { email: string },
+    formikHelpers: FormikHelpers<{ email: string }>,
+  ) => {
+    formikHelpers.resetForm();
+  };
+  return (
+    <>
+      <AuthTitle>Відновити пароль</AuthTitle>
+
+      <Description>
+        Для відновлення паролю введіть Вашу електронну адресу. Вам надійде лист із
+        посиланням для зміни паролю
+      </Description>
+
+      <RestoreForm handleSubmit={handleSubmit} validationSchema={validationSchema}>
+        <FormContainer>
+          <EmailField />
+
+          <SubmitButton>Надіслати</SubmitButton>
+        </FormContainer>
+      </RestoreForm>
+    </>
+  );
+};
