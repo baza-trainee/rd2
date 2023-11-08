2cd7046be5da5b8e412df5acfa4e57841f628bc6
VadimToma <https://github.com/TomaVadim>
2023-11-08 15:44:27 +0200

    feat: update restore with form

--- src/pages/Restore.tsx
+++ src/pages/Restore.tsx
@@ -1,15 +1,19 @@
 import { AuthContainer } from "features/components/auth/AuthContainer/AuthContainer";
 import { AuthTitle } from "features/components/auth/AuthTitle/AuthTitle";
 import { Description } from "features/components/restore/Description/Description";
+import { RestoreForm } from "features/components/restore/RestoreForm/RestoreForm";
 
 export const Restore = (): JSX.Element => {
   return (
     <AuthContainer>
       <AuthTitle>Відновити пароль</AuthTitle>
+
       <Description>
         Для відновлення паролю введіть Вашу електронну адресу. Вам надійде лист із
         посиланням для зміни паролю
       </Description>
+
+      <RestoreForm></RestoreForm>
     </AuthContainer>
   );
 };
