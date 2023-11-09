2a6814c879c8ea01d288234642c526dbbfbfb5e1
Lethteria <Lethteria@gmail.com>
2023-11-09 22:09:11 +0200

    feat: create ua translation json file

--- src/components/About/Advantages/Advantages.tsx
+++ src/components/About/Advantages/Advantages.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 // import Typography from "@mui/material/Typography";
+import {useTranslation} from "react-i18next";
 
 import advantageList from "./advantageList";
+
 import {
   Container, DescWrap, Divider, Desc, Title, AdvantageList, AdvantageDescBox,
   AdvantageCard, AdvantageImgBox, AdvantageImg, AdvantageTitle, SectionWrapper,
 } from "./Advantages.styled";
 
 const Advantages: React.FC = () => {
+
+  const { t } = useTranslation();
+
+  const advantageDescArr = t("advantages_block.items", { returnObjects: true });
+
   return <Container>
     <SectionWrapper>
       <DescWrap>
@@ -23,13 +30,13 @@ const Advantages: React.FC = () => {
         </Desc>
       </DescWrap>
       <AdvantageList>
-        {advantageList.map(({ id, title, desc, img }) =>{
+        {advantageList.map(({ id, title, desc, img }, index) =>{
           return <AdvantageCard key={id}>
             <AdvantageImgBox>
               <AdvantageImg src={img} alt={title} />
             </AdvantageImgBox>
             <AdvantageDescBox>
-              <AdvantageTitle>{title}</AdvantageTitle>
+              <AdvantageTitle>{title}{/*advantageDescArr[index].title*/}</AdvantageTitle>
               <p>{desc}</p>
             </AdvantageDescBox>
           </AdvantageCard>;
--- src/components/About/MainTasks/MainTasks.tsx
+++ src/components/About/MainTasks/MainTasks.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 
+import {useTranslation} from "react-i18next";
+
 import { Container, SectionWrapper, Title, TaskList, TaskIcon,
   Task, TaskDesc} from "./MainTasks.styled";
+
 import taskList from "./taskList";
 
 const MainTasks: React.FC = () => {
+
+  const { t } = useTranslation();
+
+  const tasksDescArr = t("main_tasks_block.desc", { returnObjects: true });
+
   return <Container>
     <SectionWrapper>
       <Title>Основні завдання</Title>
       <TaskList>
-        {taskList.map(({ id, icon, desc }) => {
+        {taskList.map(({ id, icon, desc }, index) => {
           return <Task key={id}>
             <TaskIcon src={icon} alt="task icon" />
-            <TaskDesc>{desc}</TaskDesc>
+            <TaskDesc>{tasksDescArr[index]}</TaskDesc>
           </Task>;
         })}
       </TaskList>
