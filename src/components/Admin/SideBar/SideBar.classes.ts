7a32f77afb8e2fd24709d812d11afc67236f8b27
VadimToma <https://github.com/TomaVadim>
2023-11-11 17:21:49 +0200

    feat: create admin sidebar

---
+++ src/components/Admin/SideBar/SideBar.classes.ts
@@ -0,0 +1,13 @@
+import { SxProps } from "@mui/material";
+
+const DrawerStyles: SxProps = {
+  width: "322px",
+  flexShrink: 0,
+  "& .MuiPaper-root": {
+    position: "static",
+    bgcolor: "#285298",
+    color: "#fff",
+  },
+};
+
+export { DrawerStyles };
--- src/routes/layouts/AdminLayout.tsx
+++ src/routes/layouts/AdminLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { App } from "components/App";
 import { AuthHeader } from "components/Auth/AuthHeader/AuthHeader";
 import { AdminContainer } from "components/Admin/AdminContainer/AdminContainer";
+import { ToolBar } from "components/Admin/SideBar/SideBar";
 
 export const AdminLayout = () => {
   return (
@@ -10,6 +11,8 @@ export const AdminLayout = () => {
       <AuthHeader />
 
       <AdminContainer>
+        <ToolBar />
+
         <Outlet />
       </AdminContainer>
     </App>
