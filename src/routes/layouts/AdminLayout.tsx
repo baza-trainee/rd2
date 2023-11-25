ad7c223006a1c7793f0f56ca4312f875eeb301d1
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-11-25 21:38:15 +0200

    fix: fix problem with path

--- src/routes/layouts/AdminLayout.tsx
+++ src/routes/layouts/AdminLayout.tsx
@@ -3,9 +3,8 @@ import { Outlet } from "react-router-dom";
 import { App } from "App";
 import { AuthHeader } from "components/Auth/AuthHeader/AuthHeader";
 import { AdminContainer } from "components/Admin/AdminContainer/AdminContainer";
-import { SideBar } from "components/Admin/sidebar/SideBar/SideBar";
-
 import { AdminPageContainer } from "components/Admin/AdminPageContainer/AdminPageContainer";
+import { SideBar } from "components/Admin/sidebar/SideBar/SideBar";
 
 export const AdminLayout = () => {
   return (
