73746be9c4197ebf994aefaf2153ff29787f0bb8
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-11-30 15:41:07 +0200

    feat: add tests for helpers

---
+++ src/helpers/getSlideCount.test.ts
@@ -0,0 +1,16 @@
+import { getSlideCount } from "helpers/getSlideCount";
+
+describe("getSlideCount", () => {
+  it("when isAboveXl is true should return 3", () => {
+    expect(getSlideCount(false, true)).toBe(3);
+  });
+  it("when isAboveMd is true should return 2", () => {
+    expect(getSlideCount(true, false)).toBe(2);
+  });
+  it("when both of arguments are false should return 1", () => {
+    expect(getSlideCount(false, false)).toBe(1);
+  });
+  it("when both of arguments are true should return 3", () => {
+    expect(getSlideCount(true, true)).toBe(3);
+  });
+});
