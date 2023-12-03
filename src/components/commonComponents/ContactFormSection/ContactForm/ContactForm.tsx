f64f0e304b54e5fce02ef21d57a059c66e0e2dc6
EVGENY4510 <lentiay4510z@gmail.com>
2023-12-03 11:57:12 +0200

    chore fixed validation bug in the contact form

--- src/components/commonComponents/ContactFormSection/ContactForm/ContactForm.tsx
+++ src/components/commonComponents/ContactFormSection/ContactForm/ContactForm.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { useFormik } from "formik";
 import { Button } from "@mui/material";
 
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 import { contactValuesType } from "types/typeContactInitialValues";
 
-import {contactSchema} from "./updateValidationTranslation";
+import { contactSchema } from "./updateValidationTranslation";
 
-import { FormEl, FieldContainer, FieldLabel, Input, Textarea, FormError } from "./Form.styled";
+import {
+  FormEl,
+  FieldContainer,
+  FieldLabel,
+  Input,
+  Textarea,
+  FormError,
+} from "./Form.styled";
 
 interface ContactFormProps {
   openModal: () => void;
@@ -22,87 +29,97 @@ const initialValues: contactValuesType = {
   message: "",
 };
 
-const ContactForm: React.FC<ContactFormProps> = ({openModal}) => {
-
+const ContactForm: React.FC<ContactFormProps> = ({ openModal }) => {
   const { t } = useTranslation();
 
-  const {values, errors, touched, handleBlur, handleChange, handleSubmit, resetForm} = useFormik({
-    initialValues: initialValues,
-    validationSchema: contactSchema,
-    onSubmit: ({ name, surname, phone, email, message }) => {
-      // send to email
-      const emailData = {
-        name,
-        surname,
-        phone,
-        email,
-        message,
-      };
-      // eslint-disable-next-line
-      console.log(emailData);
-      // await sendEmail(emailData);
-      resetForm();
-      openModal();
-    },
-  });
-  return <FormEl onSubmit={handleSubmit}>
-    <FieldContainer>
-      <FieldLabel>{t("contact_form.name")}</FieldLabel>
-      <Input type="text"
-        name="name"
-        onChange={handleChange}
-        value={values.name}
-        onBlur={handleBlur}
-        className={touched.name && errors.name ? "error" : ""}
-      />
-      {errors.name && touched.name && <FormError>{errors.name}</FormError>}
-    </FieldContainer>
-    <FieldContainer>
-      <FieldLabel>{t("contact_form.surname")}</FieldLabel>
-      <Input type="text"
-        name="surname"
-        onChange={handleChange}
-        value={values.surname}
-        onBlur={handleBlur}
-        className={errors.surname && touched.surname ? "error" : ""}
-      />
-      {errors.surname && touched.surname && <FormError>{errors.surname}</FormError>}
-    </FieldContainer>
-    <FieldContainer>
-      <FieldLabel>{t("contact_form.phone")}</FieldLabel>
-      <Input type="tel"
-        name="phone"
-        onChange={handleChange}
-        value={values.phone}
-        onBlur={handleBlur}
-        className={errors.phone && touched.phone ? "error" : ""}
-      />
-      {errors.phone && touched.phone && <FormError>{errors.phone}</FormError>}
-    </FieldContainer>
-    <FieldContainer>
-      <FieldLabel>{t("contact_form.email")}</FieldLabel>
-      <Input type="email"
-        name="email"
-        onChange={handleChange}
-        value={values.email}
-        onBlur={handleBlur}
-        className={errors.email && touched.email ? "error" : ""}
-      />
-      {errors.email && touched.email && <FormError>{errors.email}</FormError>}
-    </FieldContainer>
-    <FieldContainer>
-      <FieldLabel>{t("contact_form.message")}</FieldLabel>
-      <Textarea name="message"
-        onChange={handleChange}
-        value={values.message}
-        onBlur={handleBlur}
-        className={errors.message && touched.message ? "error" : ""}
-      />
-      {errors.message && touched.message && <FormError>{errors.message}</FormError>}
-    </FieldContainer>
-    <Button fullWidth variant="contained" type="submit" style={{marginTop: "32px"}}>
-      {t("buttons.send")}
-    </Button>
-  </FormEl>;
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit, resetForm } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: contactSchema,
+      onSubmit: ({ name, surname, phone, email, message }) => {
+        // send to email
+        const emailData = {
+          name,
+          surname,
+          phone,
+          email,
+          message,
+        };
+        // eslint-disable-next-line
+        console.log(emailData);
+
+        // await sendEmail(emailData);
+        resetForm();
+        openModal();
+      },
+    });
+
+  return (
+    <FormEl onSubmit={handleSubmit}>
+      <FieldContainer>
+        <FieldLabel>{t("contact_form.name")}</FieldLabel>
+        <Input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={values.name}
+          onBlur={handleBlur}
+          className={touched.name && errors.name ? "error" : ""}
+        />
+        {errors.name && touched.name && <FormError>{errors.name}</FormError>}
+      </FieldContainer>
+      <FieldContainer>
+        <FieldLabel>{t("contact_form.surname")}</FieldLabel>
+        <Input
+          type="text"
+          name="surname"
+          onChange={handleChange}
+          value={values.surname}
+          onBlur={handleBlur}
+          className={errors.surname && touched.surname ? "error" : ""}
+        />
+        {errors.surname && touched.surname && <FormError>{errors.surname}</FormError>}
+      </FieldContainer>
+      <FieldContainer>
+        <FieldLabel>{t("contact_form.phone")}</FieldLabel>
+        <Input
+          type="tel"
+          name="phone"
+          placeholder="380003003030"
+          onChange={handleChange}
+          value={values.phone}
+          onBlur={handleBlur}
+          className={errors.phone && touched.phone ? "error" : ""}
+        />
+        {errors.phone && touched.phone && <FormError>{errors.phone}</FormError>}
+      </FieldContainer>
+      <FieldContainer>
+        <FieldLabel>{t("contact_form.email")}</FieldLabel>
+        <Input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={values.email}
+          onBlur={handleBlur}
+          className={errors.email && touched.email ? "error" : ""}
+        />
+        {errors.email && touched.email && <FormError>{errors.email}</FormError>}
+      </FieldContainer>
+      <FieldContainer>
+        <FieldLabel>{t("contact_form.message")}</FieldLabel>
+        <Textarea
+          name="message"
+          onChange={handleChange}
+          value={values.message}
+          onBlur={handleBlur}
+          className={errors.message && touched.message ? "error" : ""}
+        />
+        {errors.message && touched.message && <FormError>{errors.message}</FormError>}
+      </FieldContainer>
+      <Button fullWidth variant="contained" type="submit" style={{ marginTop: "32px" }}>
+        {t("buttons.send")}
+      </Button>
+    </FormEl>
+  );
 };
 export default ContactForm;
--- src/components/commonComponents/ContactFormSection/ContactForm/contactFormValidationSchema.ts
+++ src/components/commonComponents/ContactFormSection/ContactForm/contactFormValidationSchema.ts
@@ -1,51 +1,52 @@
 import * as yup from "yup";
+const phoneRegExp =
+  /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
+function CreateContactSchema(data: any) {
+  const contactSchema = yup.object().shape({
+    name: yup
+      .string()
+      .trim()
+      .matches(/^[-\sA-Za-zа-яА-ЯіІїЇґҐёЁєЄ]+$/, data.onlyLetter)
+      .min(2, data.inputMinLength)
+      .max(50, data.inputMaxLength)
+      .required(data.requiredName),
+    surname: yup
+      .string()
+      .trim()
+      .matches(/^[-\sA-Za-zа-яА-ЯіІїЇґҐёЁєЄ]+$/, data.onlyLetter)
+      .min(2, data.inputMinLength)
+      .max(50, data.inputMaxLength)
+      .required(data.requiredSurname),
+    email: yup
+      .string()
+      .matches(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+        data.validationError,
+      )
+      .test("no-cyrillic", data.validationError, function (value) {
+        if (value) {
+          const domain = value.substring(value.lastIndexOf("@") + 1);
+          const secondDomain = domain.split(".")[0];
+          const latinRegex = /^[a-zA-Z]+$/;
+          return latinRegex.test(secondDomain);
+        }
+        return true;
+      })
+      .max(50, data.inputMaxLength)
+      .required(data.requiredEmail),
+    phone: yup
+      .string()
+      .matches(phoneRegExp, data.validationError)
+      .max(12, "Максимум 12 символів")
+      .required(data.requiredPhone),
+    message: yup
+      .string()
+      .min(10, data.messageMinLength)
+      .max(300, data.messageMaxLength)
+      .required(data.requiredField),
+  });
 
-function CreateContactSchema (data:any) {
-    const contactSchema = yup.object().shape({
-        name: yup
-            .string()
-            .trim()
-            .matches(/^[-\sA-Za-zа-яА-ЯіІїЇґҐёЁєЄ]+$/, data.onlyLetter)
-            .min(2, data.inputMinLength)
-            .max(50, data.inputMaxLength)
-            .required(data.requiredName),
-        surname: yup
-            .string()
-            .trim()
-            .matches(/^[-\sA-Za-zа-яА-ЯіІїЇґҐёЁєЄ]+$/,  data.onlyLetter)
-            .min(2, data.inputMinLength)
-            .max(50, data.inputMaxLength)
-            .required(data.requiredSurname),
-        email: yup
-            .string()
-            .matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
-                data.validationError)
-            .test("no-cyrillic", data.validationError, function (value) {
-                if (value) {
-                    const domain = value.substring(value.lastIndexOf("@") + 1);
-                    const secondDomain = domain.split(".")[0];
-                    const latinRegex = /^[a-zA-Z]+$/;
-                    return latinRegex.test(secondDomain);
-                }
-                return true;
-            })
-            .max(50, data.inputMaxLength)
-            .required(data.requiredEmail),
-        phone: yup
-            .string()
-            .required(data.requiredPhone),
-        message: yup
-            .string()
-            .min(10, data.messageMinLength)
-            .max(300, data.messageMaxLength)
-            .required(data.requiredField),
-    });
-
-    return contactSchema;
-
+  return contactSchema;
 }
 
-
-export {CreateContactSchema}
-
-
+export { CreateContactSchema };
