c9c537f2a03e6a633c302890c8dfd81b6727537e
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-04 15:10:43 +0200

    chore: update page reports with test query component

--- src/pages/admin/Reports.tsx
+++ src/pages/admin/Reports.tsx
@@ -1,11 +1,15 @@
 import Typography from "@mui/material/Typography";
 
+import { TestQuery } from "components/TestQuery/TestQuery";
+
 export const Reports = () => {
   return (
     <>
       <Typography variant="h4" component="h4" color="info.main">
         Звітність
       </Typography>
+
+      <TestQuery />
     </>
   );
 };
