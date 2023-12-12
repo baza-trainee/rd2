c4a4b55a6c519d536747b9da45d67c36674c65f2
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-13 00:25:40 +0200

    chore: remove unnecesary immports

--- src/components/Auth/AuthForm/AuthForm.tsx
+++ src/components/Auth/AuthForm/AuthForm.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+
 import { Form, Formik, FormikHelpers } from "formik";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
@@ -7,22 +9,20 @@ import { FormValues } from "types/formValues";
 import { UsernameField } from "components/Auth/UsernameField/UsernameField";
 import { PasswordField } from "components/Auth/PasswordField/PasswordField";
 import { ForgetPassword } from "components/Auth/ForgetPassword/ForgetPassword";
-
 import { validationSchema } from "components/Auth/AuthForm/validationSchema";
-
-import { FetchAuthCredentials } from "api/adminAuth";
-
 import { AuthContext } from "routes/layouts/Authorization";
 import { signIn } from "api/signIn";
-import { useContext } from "react";
 
 export const AuthForm = () => {
   const navigate = useNavigate();
   const { getAccessToken } = new AccessTokenService();
   const { setIsLoggedIn } = useContext(AuthContext);
 
-  const handleSubmit = (value: FormValues, formikHelpers: FormikHelpers<FormValues>) => {
-    signIn(value)
+  const handleSubmit = (
+    credentials: FormValues,
+    formikHelpers: FormikHelpers<FormValues>,
+  ) => {
+    signIn(credentials)
       .then((_) => {
         if (getAccessToken()) {
           setIsLoggedIn(true);
