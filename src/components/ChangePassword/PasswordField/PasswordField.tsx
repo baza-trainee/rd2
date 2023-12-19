1ff19ab1092e98d71b54095f6113a5fb17886872
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-19 18:58:20 +0200

    feat: add ability to change visibility text in input

--- src/components/ChangePassword/PasswordField/PasswordField.tsx
+++ src/components/ChangePassword/PasswordField/PasswordField.tsx
@@ -1,17 +1,27 @@
-import { Box, FilledInput, Typography } from "@mui/material";
+import { Box, FilledInput, IconButton, InputAdornment, Typography } from "@mui/material";
 import { ErrorMessage, Field } from "formik";
 
 import {
   BoxStyles,
   InputStyles,
 } from "components/ChangePassword/PasswordField/PasswordField.classes";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 interface Props {
   labelText: string;
-  name: "oldPassword" | "newPassword" | "newPasswordRepeat";
+  name: "password" | "new_password" | "confirm_password";
 }
 
 export const PasswordField = ({ labelText, name }: Props) => {
+  const [isVisiblePassword, setIsVisiblePassword] = useState<boolean>(false);
+
+  const handleChangePasswordVisibility = () => {
+    setIsVisiblePassword((prev) => !prev);
+  };
+
+  const inputType = isVisiblePassword ? "text" : "password";
+
   return (
     <Box sx={BoxStyles}>
       <Typography fontSize="h5" component="label" htmlFor={name}>
@@ -23,8 +33,16 @@ export const PasswordField = ({ labelText, name }: Props) => {
         disableUnderline
         variant="filled"
         fullWidth
-        type="password"
+        type={inputType}
         name={name}
+        aria-label="toggle password visibility"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton onClick={handleChangePasswordVisibility} edge="end">
+              {isVisiblePassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }
       />
       <ErrorMessage className="error-message" name={name} component="div" />
     </Box>
