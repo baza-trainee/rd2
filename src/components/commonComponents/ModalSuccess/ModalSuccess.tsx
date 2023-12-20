b7bf49152c05e163dba060621475558e002f5175
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-20 19:01:47 +0200

    chore: pretting

--- src/components/commonComponents/ModalSuccess/ModalSuccess.tsx
+++ src/components/commonComponents/ModalSuccess/ModalSuccess.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 import CloseIcon from "@mui/icons-material/Close";
 
-import {Box, IconButton, Modal, Typography} from "@mui/material";
+import { Box, IconButton, Modal, Typography } from "@mui/material";
 
-import {IconButtonStyles, ModalStyles} from "./ModalSuccess.classes";
-
-
+import { IconButtonStyles, ModalStyles } from "./ModalSuccess.classes";
 
 interface Props {
   children: string;
@@ -18,11 +16,7 @@ export const ModalSuccess = ({ children, isOpenModal, handleCloseModal }: Props)
   return (
     <Modal open={isOpenModal} onClose={handleCloseModal}>
       <Box sx={ModalStyles}>
-        <IconButton
-          sx={IconButtonStyles}
-          aria-label="close"
-          onClick={handleCloseModal}
-        >
+        <IconButton sx={IconButtonStyles} aria-label="close" onClick={handleCloseModal}>
           <CloseIcon />
         </IconButton>
 
