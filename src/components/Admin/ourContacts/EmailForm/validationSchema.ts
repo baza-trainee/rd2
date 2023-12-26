55c623379eb8603c4a62536e869a1b7e645e2693
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-26 10:40:27 +0200

    fix: resolve problems with validation email

--- src/components/Admin/ourContacts/EmailForm/validationSchema.ts
+++ src/components/Admin/ourContacts/EmailForm/validationSchema.ts
@@ -1,14 +1,17 @@
 /* eslint-disable max-len */
 import { object, string, ref } from "yup";
 
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
 export const validationSchema = object({
   currentEmail: string(),
   newEmail: string()
     .email("введіть вірний email")
-    .required("введіть email")
     .test("not-same", "email адреси не мають співпадати", function (value) {
       const currentEmail = this.resolve(ref("currentEmail"));
 
       return value !== currentEmail;
-    }),
+    })
+    .matches(emailRegex, "введіть вірний email")
+    .required("введіть email"),
 });
