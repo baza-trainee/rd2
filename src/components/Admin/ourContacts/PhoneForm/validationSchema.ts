5a06d3825669ff86068e000293cda81992240abb
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-26 13:47:56 +0200

    fix: update phone number validation

--- src/components/Admin/ourContacts/PhoneForm/validationSchema.ts
+++ src/components/Admin/ourContacts/PhoneForm/validationSchema.ts
@@ -6,9 +6,12 @@ const phoneRegExp = /^(\+?\d{1,3})?[ -]?\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4}$/;
 export const validationSchema = object({
   currentNumber: string(),
   newNumber: string()
-    .min(10, "номер має містити не менше 10 цифр")
-    .max(13, "номер має містити не більше 12 цифр та знаку +")
-    .matches(phoneRegExp, "не валідний номер")
+    .matches(phoneRegExp, "номер має містити знак + та 12 цифр")
+    .min(13, "номер має містити знак + та 12 цифр")
+    .max(13, "номер має містити знак + та 12 цифр")
+    .test("start from +", "номер має починатись зі знаку +", function (value) {
+      return value?.slice(0, 1).includes("+");
+    })
     .test("not-same", "номери не мають співпадати", function (value) {
       const currentNumber = this.resolve(ref("currentNumber"));
       return value !== currentNumber;
