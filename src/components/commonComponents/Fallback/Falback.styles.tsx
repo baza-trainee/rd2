066888be6342f754b2c1052610591fa276daf837
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2023-12-31 12:15:09 +0200

    chore: change background color

--- src/components/commonComponents/Fallback/Falback.styles.tsx
+++ src/components/commonComponents/Fallback/Falback.styles.tsx
@@ -1,26 +1,25 @@
 import styled from "@emotion/styled";
 
-import {Box} from "@mui/material";
-
 type FallbackWrapProps = {
-    blockType: boolean
-}
+  blockType: boolean;
+};
+
 const FallbackWrap = styled.div<FallbackWrapProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0,0,0,0.15);
-  
-  flex-grow: ${props => (!props.blockType && "2")};;
-  //min-height: ${props => (!props.blockType && "calc(100vh - 129px)")} ;
+  background-color: rgba(0, 0, 0, 0);
 
-  position: ${props => (props.blockType && "absolute" )};
-  top: ${props => (props.blockType && "0" )};
-  left: ${props => (props.blockType && "0" )};
-  z-index: ${props => (props.blockType && "100" )};
-  width: ${props => (props.blockType && "100%" )};
-  height: ${props => (props.blockType ? "100%" : "100vh")};
-  border-radius: ${props => (props.blockType && "15px" )};
+  flex-grow: ${(props) => !props.blockType && "2"};
+  //min-height: ${(props) => !props.blockType && "calc(100vh - 129px)"} ;
+
+  position: ${(props) => props.blockType && "absolute"};
+  top: ${(props) => props.blockType && "0"};
+  left: ${(props) => props.blockType && "0"};
+  z-index: ${(props) => props.blockType && "100"};
+  width: ${(props) => props.blockType && "100%"};
+  height: ${(props) => (props.blockType ? "100%" : "100vh")};
+  border-radius: ${(props) => props.blockType && "15px"};
 `;
 
-export {FallbackWrap}
+export { FallbackWrap };
