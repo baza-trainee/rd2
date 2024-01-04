786472e9abed855ab597b50ec521ddfd5a252dc9
Lethteria <Lethteria@gmail.com>
2024-01-05 00:47:01 +0200

    fix data display in feedback messages

--- src/helpers/admin/mapFeedbackMessages.ts
+++ src/helpers/admin/mapFeedbackMessages.ts
@@ -1,12 +1,25 @@
 import {Messages, UserMessage} from "../../types/typeFeedbackUserDetails";
 
+const showDate = (number: number) => {
+
+  let fixNumber:string = number.toString();
+  if (fixNumber.length < 2) fixNumber = "0" + fixNumber ;
+
+  return fixNumber;
+}
+
 export const mapFeedbackMessages = (messages:Messages[]):UserMessage[] => {
   return messages.map((mes) => {
 
     const newDate = new Date(mes.created_at);
-    const mesDate: string =
-            `${newDate.getDate()}.${newDate.getMonth()}.${newDate.getFullYear()}
-                ${newDate.getHours()}:${newDate.getMinutes()}`;
+
+    const day = showDate( newDate.getDate() );
+    const month = showDate(newDate.getMonth()+1);
+    const year = showDate( newDate.getFullYear() );
+    const hours = showDate( newDate.getHours() );
+    const min = showDate ( newDate.getMinutes() );
+
+    const mesDate: string = `${day}.${month}.${year} ${hours}:${min}`;
 
     return {
       message: mes.msg,
