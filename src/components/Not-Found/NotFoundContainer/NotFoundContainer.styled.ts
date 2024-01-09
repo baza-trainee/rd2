b7b5c27290a669b4dcec097a164b4832b0b2c264
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-01-09 20:09:44 +0200

    fix: change styles of fixed container

--- src/components/Not-Found/NotFoundContainer/NotFoundContainer.styled.ts
+++ src/components/Not-Found/NotFoundContainer/NotFoundContainer.styled.ts
@@ -10,17 +10,20 @@ const SectionWrapper = styled.div`
   flex-direction: column;
   align-items: center;
 
-  width: 360px;
-  padding: 20px 16px 108px 16px;
-  margin: 0 auto;
+  /* width: 360px; */
+  padding-block: 20px 16px;
+  /* padding: 20px 16px 108px 16px; */
+  /* margin: 0 auto; */
 
   @media (min-width: 768px) {
-    width: 768px;
-    padding: 80px 184px 94px;
+    /* width: 768px; */
+    padding-block: 80px 94px;
+    /* padding: 80px 184px 94px; */
   }
-  @media (min-width: 1440px) {
-    width: 1440px;
-    padding: 60px 521px 94px;
+  @media (min-width: 1280px) {
+    /* width: 1280px; */
+    padding-block: 60px 94px;
+    /* padding: 60px 521px 94px; */
   }
 `;
 
