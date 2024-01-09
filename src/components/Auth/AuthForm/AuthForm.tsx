ce9af381d3cf5d2d9c8e8fb321e5862f1cb6fbcc
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-01-09 20:42:45 +0200

    chore: update auth logic with hook useAuthorization

--- src/components/Auth/AuthForm/AuthForm.tsx
+++ src/components/Auth/AuthForm/AuthForm.tsx
@@ -1,43 +1,21 @@
 import { Form, Formik, FormikHelpers } from "formik";
-import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
-import { useMutation } from "react-query";
 
-import { AccessTokenService } from "services/AccessTokenService";
 import { FormValues } from "types/formValues";
 import { UsernameField } from "components/Auth/UsernameField/UsernameField";
 import { PasswordField } from "components/Auth/PasswordField/PasswordField";
 import { ForgetPassword } from "components/Auth/LinkForgotPassword/LinkForgotPassword";
 import { validationSchema } from "components/Auth/AuthForm/validationSchema";
 import { RequestFallback } from "components/commonComponents/RequestFallback/RequestFallback";
-import { signIn } from "api/signIn";
-import { checkIsTokenValid } from "helpers/auth/checkIsTokenValid";
+import { useAuthorization } from "api/query-hooks/useAuthorization";
 
 interface Props {
   handleIsOpenModal: () => void;
 }
 
 export const AuthForm = ({ handleIsOpenModal }: Props) => {
-  const navigate = useNavigate();
-  const { getAccessToken } = new AccessTokenService();
-
-  const mutation = useMutation((credentials: FormValues) => signIn(credentials), {
-    onSuccess: () => {
-      const isAuthorizated = checkIsTokenValid(getAccessToken());
-
-      if (isAuthorizated) {
-        navigate("/admin");
-      } else {
-        throw new Error("invalid credentials");
-      }
-    },
-
-    onError: () => {
-      handleIsOpenModal();
-    },
-  });
-
-  const { isLoading } = mutation;
+  const mutation = useAuthorization(handleIsOpenModal);
+  const { isLoading } = useAuthorization();
 
   const handleSubmit = (credentials: FormValues, _: FormikHelpers<FormValues>) => {
     mutation.mutate(credentials);
