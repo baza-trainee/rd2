3e2a87e37961e0adea7f1a7cb8dc48c272b315ba
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-01-09 12:59:28 +0200

    fix: change validation schemas

--- src/components/Admin/ChangePassword/ChangePasswordForm/validationSchema.ts
+++ src/components/Admin/ChangePassword/ChangePasswordForm/validationSchema.ts
@@ -1,14 +1,16 @@
 /* eslint-disable max-len */
 import { object, ref, string } from "yup";
 
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{6,}/;
+const passwordRegex =
+  /([a-z]+[A-Z]+[0-9]+|[a-z]+[0-9]+[A-Z]+|[A-Z]+[a-z]+[0-9]+|[A-Z]+[0-9]+[a-z]+|[0-9]+[a-z]+[A-Z]+|[0-9]+[A-Z]+[a-z]+)/;
 
 export const validationSchema = object({
   password: string().required("введіть старий пароль"),
   new_password: string()
+    .min(6, "мінімально шість знаків")
     .matches(
       passwordRegex,
-      "пароль має містити тільки латинські літери (мінімум 8 знаків), одну велику літеру, цифру та спецсимвол !@#$%^&*",
+      "пароль має місти тільки латинські літери, мати хоча б одну букву в верхньому регістрі та цифру",
     )
     .required("введіть новий пароль"),
   confirm_password: string()
--- src/components/NewPassword/NewPasswordForm/validationSchema.ts
+++ src/components/NewPassword/NewPasswordForm/validationSchema.ts
@@ -1,13 +1,16 @@
 /* eslint-disable max-len */
 import { object, string, ref } from "yup";
 
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{6,}/;
+const passwordRegex =
+  /([a-z]+[A-Z]+[0-9]+|[a-z]+[0-9]+[A-Z]+|[A-Z]+[a-z]+[0-9]+|[A-Z]+[0-9]+[a-z]+|[0-9]+[a-z]+[A-Z]+|[0-9]+[A-Z]+[a-z]+)/;
 
 export const validationSchema = object({
-  password: string().matches(
-    passwordRegex,
-    "пароль має містити тільки латинські літери (мінімум 8 знаків), одну велику літеру, цифру та спецсимвол !@#$%^&*",
-  ),
+  password: string()
+    .min(6, "мінімально шість знаків")
+    .matches(
+      passwordRegex,
+      "пароль має місти тільки латинські літери, мати хоча б одну букву в верхньому регістрі та цифру",
+    ),
 
   confirmPassword: string()
     .oneOf([ref("password")], "пароль має співпадать")
