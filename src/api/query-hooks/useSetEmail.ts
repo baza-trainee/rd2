19d307ad2d3ef8b74d0f1543d8f700129e2bb136
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-01-09 21:19:49 +0200

    feat: create hooks useSetEmail and useSetPhone

---
+++ src/api/query-hooks/useSetEmail.ts
@@ -0,0 +1,18 @@
+import { queryClient } from "App";
+import { EmailCredentials, setNewEmail } from "api/setNewEmail";
+import { AxiosError } from "axios";
+import { useMutation } from "react-query";
+
+export const useSetEmail = (openSuccessModal: () => void, openErrorModal: () => void) =>
+  useMutation((credentials: EmailCredentials) => setNewEmail(credentials), {
+    onSuccess: () => {
+      openSuccessModal();
+
+      queryClient.invalidateQueries("email");
+    },
+    onError: (error: AxiosError) => {
+      if (error.response && error) {
+        openErrorModal();
+      }
+    },
+  });
