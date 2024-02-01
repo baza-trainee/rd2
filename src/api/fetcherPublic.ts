479635f4a9f86f048eca60d30f6f3e8f8e1d9edb
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-02-01 14:43:35 +0200

    fix: fix return value from func handleSlideToDisplay

--- src/api/fetcherPublic.ts
+++ src/api/fetcherPublic.ts
@@ -5,4 +5,3 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const fetcherPublic = axios.create({
   baseURL: BASE_URL,
 });
-
--- src/api/requests/getCurrentEmail.ts
+++ src/api/requests/getCurrentEmail.ts
@@ -1,8 +1,8 @@
-import {fetcherPublic} from "api/fetcherPublic";
+import { fetcherPublic } from "api/fetcherPublic";
 
 interface CurrentEmail {
   email: string;
 }
 
 export const getCurrentEmail = () =>
-    fetcherPublic.get<CurrentEmail>("/api/contact/contacts/emails");
+  fetcherPublic.get<CurrentEmail>("/api/contact/contacts/emails");
